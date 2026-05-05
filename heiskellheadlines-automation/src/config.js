c4a8afedbe764b2491ffe9a7dac7cc791da1b70c
Dr-e-w <drewkellerbusiness@gmail.com>
2026-05-05 12:38:57 -0400

    Add automation config loader

---
+++ heiskellheadlines-automation/src/config.js
@@ -0,0 +1,30 @@
+export function loadConfig(env = process.env) {
+  const ghostAdminApiUrl = trimTrailingSlash(env.GHOST_ADMIN_API_URL || "");
+  const ghostAdminApiKey = env.GHOST_ADMIN_API_KEY || "";
+  const ghostAdminApiVersion = env.GHOST_ADMIN_API_VERSION || "v5.0";
+  const openAiApiKey = env.OPENAI_API_KEY || "";
+  const openAiModel = env.OPENAI_MODEL || "gpt-5.2";
+  const sourceFeedUrls = parseList(env.SOURCE_FEED_URLS || "");
+  const sourceManifestPath = env.SOURCE_MANIFEST_PATH || "sources.txt";
+
+  return {
+    ghostAdminApiUrl,
+    ghostAdminApiKey,
+    ghostAdminApiVersion,
+    openAiApiKey,
+    openAiModel,
+    sourceFeedUrls,
+    sourceManifestPath
+  };
+}
+
+function trimTrailingSlash(value) {
+  return value.replace(/\/+$/, "");
+}
+
+function parseList(value) {
+  return value
+    .split(/[\n,]/)
+    .map((item) => item.trim())
+    .filter(Boolean);
+}
